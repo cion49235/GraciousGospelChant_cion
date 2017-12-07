0e073ad1c2e3d33027f70bda46279fae8d9678f0
cion49235 <cion49235@nate.com>
2017-12-07 13:07:14 +0900

    [20171207]복음성가

---
+++ src/gracious/gospel/cion/data/MainCategory_Data.java
@@ -0,0 +1,10 @@
+package gracious.gospel.cion.data;
+
+
+public class MainCategory_Data {
+	 public String id;
+	 public String num;
+	 public String subject;
+	 public String image;
+	 public String album;
+ }
